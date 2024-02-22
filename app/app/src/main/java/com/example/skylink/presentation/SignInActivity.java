640251b55a1f2e957a724f77080315b0fc72022d
Dhairya Kachhia <78507435+DhairyaKachhia@users.noreply.github.com>
2024-02-22 01:45:43 -0600

    implemented sign-in button functionality

--- app/app/src/main/java/com/example/skylink/presentation/SignInActivity.java
+++ app/app/src/main/java/com/example/skylink/presentation/SignInActivity.java
@@ -2,15 +2,31 @@ package com.example.skylink.presentation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.skylink.R;
 
 public class SignInActivity extends AppCompatActivity {
 
+    private EditText username;
+    private EditText password;
+    private Button signIn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+
+        signIn = findViewById(R.id.btnSignIn);
+
+        signIn.setOnClickListener(v -> {
+            Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
