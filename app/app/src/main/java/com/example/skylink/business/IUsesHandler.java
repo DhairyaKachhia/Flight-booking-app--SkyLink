8dbd3afe5a37debb9920d46af8a6391d5dea0036
Dhairya Kachhia <78507435+DhairyaKachhia@users.noreply.github.com>
2024-02-26 00:25:59 -0600

    rename UserHandler

--- app/app/src/main/java/com/example/skylink/business/IUsesHandler.java
+++ app/app/src/main/java/com/example/skylink/business/IUsesHandler.java
@@ -1,6 +1,6 @@
 package com.example.skylink.business;
 
-public interface UsesHandler {
+public interface IUsesHandler {
     boolean createUser(String fullName, String email, String password, String rePassword);
 
     boolean updateUserProfile(String gender, String address, String phone, String dateOfBirth, String countryOfOrigin);
