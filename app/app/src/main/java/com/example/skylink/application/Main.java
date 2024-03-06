4a60240b713202616d2707319667f314a1a8ea68
akintundemayokun <akintumm@myumanitoba.ca>
2024-03-06 16:16:57 -0600

    1. Hsqldb does not reset to original data

--- app/app/src/main/java/com/example/skylink/application/Main.java
+++ app/app/src/main/java/com/example/skylink/application/Main.java
@@ -1,7 +1,7 @@
 package com.example.skylink.application;
 
 public class Main {
-    private static String dbName="SkyLinkARS";
+    private static String dbName="DBSkyLink1";
 
     public static void main(String[] args) {
         System.out.println("All done");
