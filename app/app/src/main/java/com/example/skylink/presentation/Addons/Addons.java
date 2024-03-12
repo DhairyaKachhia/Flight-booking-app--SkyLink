bc37907e85386980325af1c73577f82276c295de
Dhairya Kachhia <78507435+DhairyaKachhia@users.noreply.github.com>
2024-03-11 23:15:54 -0500

    initial page design ready.

---
+++ app/app/src/main/java/com/example/skylink/presentation/Addons/Addons.java
@@ -0,0 +1,16 @@
+package com.example.skylink.presentation.Addons;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.skylink.R;
+
+public class Addons extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_addons);
+    }
+}
