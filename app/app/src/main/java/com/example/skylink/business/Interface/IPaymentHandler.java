050754e627ccd71d7d66ef45b1c558331f6868ae
akintundemayokun <akintumm@myumanitoba.ca>
2024-03-21 15:54:11 -0500

    Fix

--- app/app/src/main/java/com/example/skylink/business/Interface/IPaymentHandler.java
+++
@@ -1,8 +0,0 @@
-package com.example.skylink.business.Interface;
-
-import com.example.skylink.objects.Interfaces.ITripInvoice;
-
-public interface IPaymentHandler {
-
-    boolean addPayment(ITripInvoice tripInvoice, long sessionUserID);
-}
